71321e47a7441068cc10d1777328073a997cf676
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-19 17:26:48 +0530

    clean the code remove theme context for next example

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
-import Home from './components/Home'
-
 
 function App() {
 
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>React-Context-Api</h1>
-      <Home/>
+     
     </>
   )
 }
