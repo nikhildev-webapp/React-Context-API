c5a3840d787031a8d413557e99ec82083eb33604
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-19 17:11:30 +0530

    ThemeContext setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { ThemeContext } from './context/TemeContext'
 
 function App() {
 
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>React-Context-Api</h1>
-      
+   
     </>
   )
 }
